ed81ce9a2e8e45cc06810bf31bfffacda3e07996
Walter Fan (yafan) <yafan@cisco.com>
2021-06-17 17:20:20 +0800

    update example

--- examples/js/desktop_sharing_demo.js
+++ examples/js/desktop_sharing_demo.js
@@ -13,78 +13,76 @@ if (adapter.browserDetails.browser == 'firefox') {
   adapter.browserShim.shimGetDisplayMedia(window, 'screen');
 }
 
-const gdmOptions = {
-  video: {
-    cursor: "always"
-  },
-  audio: {
-    echoCancellation: true,
-    noiseSuppression: true,
-    sampleRate: 44100
-  }
+if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+    weblog("enumerateDevices() not supported.");
+} else {
+  navigator.mediaDevices.enumerateDevices()
+    .then(function(devices) {
+      devices.forEach(function(device) {
+        weblog(device.kind);
+      });
+    })
+    .catch(function(err) {
+      weblog(err.name + ": " + err.message);
+    });
 }
 
-var VideoRoom = function() {
-  this._init.apply(this, arguments);
-}
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+const videoElement = document.getElementById('localVideo');
 
-VideoRoom.prototype._init = function() {
-  this.inputVideo = window.document.createElement('video');
-  this.inputCanvas = window.document.createElement('canvas');
-  this.inputContext = this.inputCanvas.getContext('2d');
+startButton.addEventListener('click', () => {
+  const textArea = document.getElementById('gdmOptions');
+  var gdmOptions = JSON.parse(textArea.value);
+  navigator.mediaDevices.getDisplayMedia(gdmOptions)
+      .then(handleSuccess, handleError);
+});
 
-  // get rgba data from input video
-  this.inputContext.drawImage(this.inputVideo, 0, 0, this.picWidth, this.picHeight);
-  var rgbaArray = this.inputContext.getImageData(0, 0, this.picWidth, this.picHeight).data;
+stopButton.addEventListener('click', () => {
+  
+  const stream = videoElement.srcObject;
+  const tracks = stream.getTracks();
+  tracks.forEach(function(track) {
+      track.stop();
+  });
+  videoElement.srcObject = null;
+  startButton.disabled = false;
+});
+
+
+if ((navigator.mediaDevices && 'getDisplayMedia' in navigator.mediaDevices)) {
+  startButton.disabled = false;
+} else {
+  weblog('getDisplayMedia is not supported');
 }
 
 function handleSuccess(stream) {
   startButton.disabled = true;
-  const video = document.querySelector('video');
-  video.srcObject = stream;
+ 
+  videoElement.srcObject = stream;
 
 
   const videoTracks = stream.getVideoTracks();
   const audioTracks = stream.getAudioTracks();
   if (videoTracks.length > 0) {
-    weblog(`Using video device: ${videoTracks[0].label}`);
     var videoTrack = videoTracks[0];
     var trackWidth = videoTrack.getSettings().width;
     var trackHeight = videoTrack.getSettings().height;
-    weblog(`video width: ${trackWidth}, height: ${trackHeight}`);
+    weblog(`Video track: ${videoTracks[0].label}, width: ${trackWidth}, height: ${trackHeight}`);
   }
   if (audioTracks.length > 0) {
-    weblog(`Using audio device: ${audioTracks[0].label}`);
+    weblog(`Audio track: ${audioTracks[0].label}`);
   }
 
   // demonstrates how to detect that the user has stopped
   // sharing the screen via the browser UI.
   stream.getVideoTracks()[0].addEventListener('ended', () => {
-    errorMsg('The user has ended sharing the screen');
+    weblog('The user has ended sharing the screen');
     startButton.disabled = false;
   });
 }
 
 function handleError(error) {
-  errorMsg(`getDisplayMedia error: ${error.name}`, error);
+  weblog(`getDisplayMedia error: ${error.name}`, error);
 }
 
-function errorMsg(msg, error) {
-  const errorElement = document.querySelector('#errorMsg');
-  errorElement.innerHTML += `<p>${msg}</p>`;
-  if (typeof error !== 'undefined') {
-    console.error(error);
-  }
-}
-
-const startButton = document.getElementById('startButton');
-startButton.addEventListener('click', () => {
-  navigator.mediaDevices.getDisplayMedia(gdmOptions)
-      .then(handleSuccess, handleError);
-});
-
-if ((navigator.mediaDevices && 'getDisplayMedia' in navigator.mediaDevices)) {
-  startButton.disabled = false;
-} else {
-  errorMsg('getDisplayMedia is not supported');
-}
