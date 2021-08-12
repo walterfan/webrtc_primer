300359f34747ffd2b480c77321b917fa351cac9d
Walter Fan (yafan) <yafan@cisco.com>
2021-08-12 19:49:40 +0800

    udpate example

--- snippets/opencv/rgb_demo.py
+++ snippets/opencv/rgb_demo.py
@@ -1,11 +1,14 @@
 import cv2
 
-SHOW_DELAY_MS= 300000
+SHOW_DELAY_MS= 3000
 
 img = cv2.imread("../../material/xjl.png")
+
+
+print(img)
+print("[[b, g , r]]")
 print("img.item(1,2,0)=", img.item(1,2,0))
 
 cv2.imshow('xjl, ', img )
-
 cv2.waitKey(SHOW_DELAY_MS)
 cv2.destroyAllWindows()
