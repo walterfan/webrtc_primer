102673e0aa8343ff3b75268429c125e8db9adca7
Walter Fan <walterfan@qq.com>
2022-01-04 23:29:53 +0800

    update js

--- examples/js/webcodecs_demo.js
+++ examples/js/webcodecs_demo.js
@@ -20,121 +20,29 @@ RtcApp.prototype.init = function() {
     this.canvasContext = this.canvasElement.getContext("2d");
 
     this.codecsWorker = new Worker("./js/webcodecs_worker.js");
+    this.codecsWorker.onmessage = ({data}) => weblog(data);
 
-
-    const init = {
-        output: this.handleChunk.bind(this),
-        error: this.handleError.bind(this)
-      };
-      //https://developer.mozilla.org/en-US/docs/Web/API/VideoEncoder/configure
-      //avc codec string: https://www.w3.org/TR/webcodecs-avc-codec-registration/
-      let config = {
-        codec: 'avc1.42E01F', //H264: 42e01f
-        width: this.canvasWidth,
-        height: this.canvasHeight,
-        bitrate: 2_000_000, // 2 Mbps
-        framerate: 30,
-      };
-      
-      this.videoEncoder = new VideoEncoder(init);
-      this.videoEncoder.configure(config);
-
-};
-
-RtcApp.prototype.test = function() {
-    console.log("--- test ---");
-    this.codecsWorker.onmessage = ({data}) => console.log(data);
-    this.codecsWorker.postMessage(5);
+    var command = { name: "init", data: { value: 30}};
+    this.codecsWorker.postMessage(command);
 }
 
 
-RtcApp.prototype.createVideoFrame=function(timestamp) {
-    const pixelSize = 4;
-    const init = {timestamp: timestamp, codedWidth: this.canvasWidth, codedHeight: this.canvasHeight, format: 'RGBA'};
-    let data = new Uint8Array(init.codedWidth * init.codedHeight * pixelSize);
-    for (let x = 0; x < init.codedWidth; x++) {
-      for (let y = 0; y < init.codedHeight; y++) {
-        let offset = (y * init.codedWidth + x) * pixelSize;
-        data[offset] =  getRandomNum(0,255);      // Red
-        data[offset + 1] =  getRandomNum(0,255);  // Green
-        data[offset + 2] =  getRandomNum(0,255);  // Blue
-        data[offset + 3] = 0x0FF; // Alpha
-      }
-    }
-    let frame = new VideoFrame(data, init);
-    return frame;
+
+RtcApp.prototype.createVideoFrames=function(e) {
+
+    var command = { name: "createVideoFrames", data: { value: 30}};
+    this.codecsWorker.postMessage(command);
 }
 
-RtcApp.prototype.videoEncode=function(e) {
-    console.log("--- videoEncode ---");
-    //new VideoFrame(this.canvasElement, { timestamp: 0 });
-    for(var i=0; i < 30; i++) {
-        let videoFrame = this.createVideoFrame(performance.now() * 1000);
+RtcApp.prototype.encodeVideoFrames=function(e) {
 
-        this.frameCounter++;
-        const insert_keyframe = (this.frameCounter % 10) == 0;
-        this.videoEncoder.encode(videoFrame, { keyFrame: insert_keyframe });
-        videoFrame.close();
-    }
-    
+    var command = { name: "encodeVideoFrames", data: { value: 5}};
+    this.codecsWorker.postMessage(command);
 }
 
-RtcApp.prototype.handleChunk=function(chunk, metadata) {
-    console.log("handleChunk: ", chunk, metadata, " at ", performance.now());
+RtcApp.prototype.decodeVideoFrames=function(e) {
 
-    /*
-    chunk: EncodedVideoChunk
-        byteLength: 57170
-        duration: 0
-        timestamp: 0
-        type: "key"
-    metadata: decoderConfig
-        codec: "avc1.42E01F"
-        codedHeight: 480
-        codedWidth: 640
-        colorSpace: {fullRange: false, matrix: 'smpte170m', primaries: 'smpte170m', transfer: 'smpte170m'}
-        description:  ArrayBuffer(41)
-        hardwareAcceleration: "no-preference"
-    */
-    this.timepoints.push(performance.now());
-    let chunkData = new Uint8Array(chunk.byteLength);
-    chunk.copyTo(chunkData);
-    this.encodedFrames.push(chunkData);
-
-    if(this.timepoints.length >= 30) {
-        this.checkPerformance();
-    }
+    var command = { name: "decodeVideoFrames", data: { value: 5}};
+    this.codecsWorker.postMessage(command);
 }
-RtcApp.prototype.handleError = function(e) {
-    console.log("error: ", e);
-}
-
-RtcApp.prototype.videoDecode=function(e) {
-    console.log("--- videoDecode ---");
-}
-
-RtcApp.prototype.checkPerformance=function(e) {
-    console.log("--- checkPerformance ---", this.frameCounter);
-    console.log("timepoints: ",  this.timepoints);
-    weblog("Spent: ", this.timepoints[29] - this.timepoints[0], "ms for 30 video frames");
-}
-
-RtcApp.prototype.drawRandomImage=function(e) {
-    console.log("--- drawRandomImage ---");
-    if(this.countDownTimer) clearInterval(this.countDownTimer);
-    this.canvasContext.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
-
-    var imageData = this.canvasContext.createImageData(this.canvasWidth, this.canvasHeight);
-
-    for (var x = 0; x < imageData.width; x++)
-    {
-        for (var y = 0; y < imageData.height; y++)
-        {
-            setPixel(imageData, x, y, getRandomNum(0,255), getRandomNum(0,255), getRandomNum(0,255), getRandomNum(0,255));
-        }
-    }
-
-    this.canvasContext.putImageData(imageData, 0, 0);
-}
-
 
--- examples/js/webcodecs_worker.js
+++ examples/js/webcodecs_worker.js
@@ -1,36 +1,203 @@
-function factorial(n) {
-    let result = 1;
-    while(n) {
-        result *= n--;
+self.rtcWorker = new RtcWorker();
+
+self.onmessage = function(event) {
+    var command = event.data;
+    var command_name = command && command.name;
+    var command_data = command && command.data;
+    console.log("begin onmessage: ", command);
+    if (!(command_name && command_data)) {
+        return;
     }
-    return result;
+    var start_time = performance.now();
+    if (command_name.match(/init/i)) {
+        self.rtcWorker.initialize(command_data.value);
+        self.postMessage(`Did ${command_name}: ${command_data.value} in ${performance.now() - start_time} ms`);
+    } else  if (command_name.match(/createVideoFrames/i)) {
+        self.rtcWorker.createVideoFrames(command_data.value);
+        self.postMessage(`Did ${command_name}: ${command_data.value} in ${performance.now() - start_time} ms`);
+    } else  if (command_name.match(/encodeVideoFrames/i)) {
+        self.rtcWorker.encodeVideoFrames(command_data.value);
+        self.postMessage(`Did ${command_name}: ${self.rtcWorker.encodedCount} in ${performance.now() - start_time} ms`);
+    } else  if (command_name.match(/decodeVideoFrames/i)) {
+        self.rtcWorker.decodeVideoFrames(command_data.value);
+        self.postMessage(`Did ${command_name}: ${self.rtcWorker.decodedCount} in ${performance.now() - start_time} ms`);
+    }
+
+    
+};
+
+function RtcWorker() {
+ 
+    this.canvasWidth  = 1920;
+    this.canvasHeight = 1080;
+    this.frameCount = 0;
+    this.encodedCount = 0;
+    this.decodedCount = 0;
+
+    this.encodeTimepoints = [];
+    this.decodeTimepoints = [];
+
+    this.videoFrames = [];
+    this.encodedFrames = [];
+    this.decodedFrames = [];
 }
 
 
-function createVideoFrame(frameWidth, frameHeight) {
+RtcWorker.prototype.initialize = function(frameCount) {
+    console.log("--- initialize ---");
+    this.frameCount = frameCount;
+
+
+    const initOfEncoder = {
+        output: this.handleChunk.bind(this),
+        error: this.handleError.bind(this)
+      };
+
+    const initOfDecoder = {
+        output: this.handleFrame.bind(this),
+        error: this.handleError.bind(this)
+      };  
+      //https://developer.mozilla.org/en-US/docs/Web/API/VideoEncoder/configure
+      //avc codec string: https://www.w3.org/TR/webcodecs-avc-codec-registration/
+    const config = {
+        codec: 'avc1.42E01F', //H264: 42e01f
+        width: this.canvasWidth,
+        height: this.canvasHeight,
+        bitrate: 12_000_000, // 2 Mbps
+        framerate: 60
+      };
+
+      this.videoEncoder = new VideoEncoder(initOfEncoder);
+      this.videoEncoder.configure(config);
+      
+      this.videoDecoder = new VideoDecoder(initOfDecoder);
+      this.videoDecoder.configure(config);
+}
+
+RtcWorker.prototype.createVideoFrames = function() {
+    console.log("--- createVideoFrames ---");
+
+    //new VideoFrame(this.canvasElement, { timestamp: 0 });
+    for(var i=0; i < this.frameCount; i++) {
+        let videoFrame = this.createVideoFrame(performance.now() * 1000);
+        this.videoFrames.push(videoFrame);
+    }
+
+};
+
+
+
+
+RtcWorker.prototype.createVideoFrame=function(timestamp) {
     const pixelSize = 4;
-    const init = {timestamp: 0, codedWidth: frameWidth, codedHeight: frameHeight, format: 'RGBA'};
+    const init = {timestamp: timestamp, codedWidth: this.canvasWidth, codedHeight: this.canvasHeight, format: 'RGBA'};
     let data = new Uint8Array(init.codedWidth * init.codedHeight * pixelSize);
     for (let x = 0; x < init.codedWidth; x++) {
-        for (let y = 0; y < init.codedHeight; y++) {
-            let offset = (y * init.codedWidth + x) * pixelSize;
-            data[offset] = 0x7F;      // Red
-            data[offset + 1] = 0xFF;  // Green
-            data[offset + 2] = 0xD4;  // Blue
-            data[offset + 3] = 0x0FF; // Alpha
-        }
+      for (let y = 0; y < init.codedHeight; y++) {
+        let offset = (y * init.codedWidth + x) * pixelSize;
+        data[offset] =  Math.random() * 255;      // Red
+        data[offset + 1] =  Math.random() * 255;  // Green
+        data[offset + 2] =  Math.random() * 255;  // Blue
+        data[offset + 3] = Math.random() * 0x0FF; // Alpha
+      }
     }
-    return new VideoFrame(data, init);
+    let frame = new VideoFrame(data, init);
+    return frame;
 }
 
-function encodeVideoFrame(videoFrame) {
+RtcWorker.prototype.encodeVideoFrames = async function(e) {
+    console.log("--- encodeVideoFrames ---");
+    for(var videoFrame of this.videoFrames) {
+        
+        const insert_keyframe = (this.encodedCount % 10) == 0;
+        this.videoEncoder.encode(videoFrame, { keyFrame: insert_keyframe });
+        videoFrame.close();
+    }
 
-}
-
-function decodeVideoFrame(videoData) {
+    await this.videoEncoder.flush();
     
 }
 
-self.onmessage = ({data}) => {
-    self.postMessage(`${data}! = ${factorial(data)}`);
-};
+
+
+RtcWorker.prototype.handleChunk = function(chunk, metadata) {
+    //console.log("handleChunk: ", chunk, metadata, " at ", performance.now());
+    this.encodedCount++;
+    /*
+    chunk: EncodedVideoChunk
+        byteLength: 57170
+        duration: 0
+        timestamp: 0
+        type: "key"
+    metadata: decoderConfig
+        codec: "avc1.42E01F"
+        codedHeight: 480
+        codedWidth: 640
+        colorSpace: {fullRange: false, matrix: 'smpte170m', primaries: 'smpte170m', transfer: 'smpte170m'}
+        description:  ArrayBuffer(41)
+        hardwareAcceleration: "no-preference"
+    */
+    this.encodeTimepoints.push(performance.now());
+
+    let chunkData = new Uint8Array(chunk.byteLength);
+    chunk.copyTo(chunkData);
+    
+    let chunkObj = new EncodedVideoChunk({
+        timestamp: chunk.timestamp,
+        duration: chunk.duration,
+        type: chunk.type,
+        data: chunkData
+      });
+  
+    this.encodedFrames.push(chunkObj);
+
+    if(this.encodedCount >= 30) 
+        this.checkPerformance("encode");
+    
+}
+
+RtcWorker.prototype.handleFrame = function(frame) {
+    console.log("--- handleFrame ---", this.decodedCount);
+    this.decodedCount++;
+    this.decodeTimepoints.push(performance.now());
+    this.decodedFrames.push(frame);
+
+    if(this.decodedCount >= 30) 
+        this.checkPerformance("decode");
+}
+
+RtcWorker.prototype.handleError = function(e) {
+    console.log("error: ", e);
+    self.postMessage(e.message);
+}
+
+RtcWorker.prototype.decodeVideoFrames = async function() {
+    console.log("--- decodeVideoFrames ---");
+    var keyFrameCount = 0;
+    for (var chunk of this.encodedFrames) {
+        console.log("chunk=", chunk);
+        if (chunk.type === "key") keyFrameCount++;
+        if (keyFrameCount === 0) continue;
+
+        this.videoDecoder.decode(chunk);
+    }
+    await this.videoDecoder.flush();
+}
+
+RtcWorker.prototype.checkPerformance=function(category) {
+    console.log("--- checkPerformance ---", category);
+    if("encode" === category) {
+        var duration = this.encodeTimepoints[29] - this.encodeTimepoints[0];
+        //console.log("encodeTimepoints: ",  this.encodeTimepoints);
+        self.postMessage(`Spent ${duration} ms to encode ${this.encodedCount} video frames`);
+    } else {
+        var duration = this.decodeTimepoints[29] - this.decodeTimepoints[0];
+        //console.log("encodeTimepoints: ",  this.encodeTimepoints);
+        self.postMessage(`Spent ${duration} ms to decode ${this.decodedCount} video frames`);
+    }
+    
+}
+
+
+
+
