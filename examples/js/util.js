596bd597b76f14b0d59a7fb51205ecaebea707c6
Walter Fan (yafan) <yafan@cisco.com>
2022-01-05 09:50:33 +0800

    update example

--- examples/js/util.js
+++ examples/js/util.js
@@ -171,6 +171,30 @@ function getBrowserName(brownserInfo) {
       "majorVersion=", majorVersion);
 }
 
+function getBrowserName(){
+    var ua = navigator.userAgent;
+    var tem;
+    var M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if (/trident/i.test(M[1])){
+        tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return { 'browser': 'IE', 'version': tem[1] || '' };
+    }
+    if (M[1] === 'Chrome'){
+        tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if(tem != null) {
+            return { 'browser': tem[1].replace('OPR', 'Opera'), 'version': tem[2] };
+        }
+        if (ua.indexOf("CrOS") >= 0) {
+            M[1] = "Chromebook";
+        }
+    }
+    M = M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if ((tem= ua.match(/version\/(\d+)/i))!== null) {
+        M.splice(1, 1, tem[1]);
+    }
+    return { 'browser': M[0], 'version': M[1] };
+}
+
 createVideoElement = function(elementId, parentId, width) {
     
     var element = window.document.createElement('video');
